076f625dd6357a6a284da38511f3fb1a64ce5d93
shoulderkarlsson <shoulderkarlsson@gmail.com>
2017-10-26 00:11:17 +0200

    fix: added date and month to clock

--- src/widgets/clock.js
+++ src/widgets/clock.js
@@ -1,6 +1,10 @@
 const clock = () => {
   const d = new Date()
-  return `${d.getHours()}:${d.getMinutes()}`
+  const hours = d.getHours()
+  const minutes = d.getMinutes()
+  const date = d.getDate()
+  const month = d.getMonth() + 1
+  return `${hours}:${minutes} - ${date}/${month}`
 }
 
 export const clockWidget = clock
