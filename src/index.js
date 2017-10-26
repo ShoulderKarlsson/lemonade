919dd0aa0e6055e31c7843a1fc5d884681d8e107
shoulderkarlsson <shoulderkarlsson@gmail.com>
2017-10-26 21:13:03 +0200

    fix: prettier and including volumeWidget

--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ import { clockWidget } from './widgets/clock'
 import { networkWidget } from './widgets/network'
 import { batteryWidget } from './widgets/battery'
 import { workspaceWidget } from './widgets/workspace'
-
-// amixer get Master | awk '$0~/%/{print $4}' | tr -d '[]'
+import { volumeWidget } from './widgets/volume'
 
 async function main() {
-  const widgets = [workspaceWidget, clockWidget, networkWidget, batteryWidget]
+  const widgets = [
+    workspaceWidget,
+    clockWidget,
+    networkWidget,
+    volumeWidget,
+    batteryWidget,
+  ]
+
   const results = await Promise.all(widgets.map(w => w()))
-  const barBuffer = results.reduce((str, widget) => str += ` ${widget.trim()}`, '')
+  const barBuffer = results.reduce(
+    (str, widget) => (str += ` ${widget.trim()}`),
+    '',
+  )
 
   // printing to console => lemonbar uses output
   console.log(barBuffer.trim())
