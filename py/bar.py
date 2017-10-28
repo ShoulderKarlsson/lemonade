f95bfcb2ed2296883f502790c3edab59f07ea8e3
shoulderkarlsson <shoulderkarlsson@gmail.com>
2017-10-28 13:01:59 +0200

    fix(python): favoring reduce when building string for bar

--- py/bar.py
+++ py/bar.py
@@ -1,18 +1,15 @@
-from widgets import battery
-
-from subprocess import call, Popen
-
-
-
+from lib import battery, volume, workspaces
 
 def main():
-  # start bar here
-  battery.battery()
+  widgets = [
+    workspaces.workspaces,
+    volume.volume,
+    battery.battery,
+  ]
 
-
+  # String buffer for bar
+  buffer = reduce(lambda acc, curr: "%s %s" % (acc, curr()), widgets, '')
+  print buffer.strip()
 
 if __name__ == '__main__':
   main()
-
-
-
